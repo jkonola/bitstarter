d57bbc7220ce5fe829e8a0a5f47452798f772cec
Ubuntu <ubuntu@ip-172-31-38-141.us-west-2.compute.internal>
2013-07-10 18:19:48 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var buffer = fs.readFileSync('index.html');
-//var output = buffer.toString;
+var output = buffer.toString;
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   response.send('Hello World 2');
